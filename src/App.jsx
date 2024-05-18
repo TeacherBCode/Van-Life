2317acd4f08b4a8873c1b81ad3179b5e345166c2
Teacher B <156999512+TeacherBCode@users.noreply.github.com>
2024-05-18 13:24:20 +0530

    SPA-(Van/About)

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,22 @@
+import React from 'react';
+import { BrowserRouter, Routes, Route, Link } from "react-router-dom"
+import Home from "./Components/Home"
+import About from "./Components/About"
+
 function App() {
   return (
-    <div>
-      <h2>Time to get started!</h2>
-    </div>
+    <BrowserRouter>
+      <header>
+        <Link className="site-logo" to="/">#VanLife</Link>
+        <nav>
+          <Link to="/about">About</Link>
+        </nav>
+      </header>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
